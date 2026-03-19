60ebff3580f651def8082b3e38878c4db1f2215c
minwoo1119 <minwoo1119@naver.com>
2026-03-19 23:02:19 +0900

    fix: 누락 엔티티 수정

--- src/people/people.module.ts
+++ src/people/people.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PersonEntity, TransactionEntity } from '../database/entities';
+import { EventEntity, PersonEntity, TransactionEntity } from '../database/entities';
 import { PeopleController } from './people.controller';
 import { PeopleService } from './people.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PersonEntity, TransactionEntity])],
+  imports: [TypeOrmModule.forFeature([PersonEntity, TransactionEntity, EventEntity])],
   controllers: [PeopleController],
   providers: [PeopleService],
 })
